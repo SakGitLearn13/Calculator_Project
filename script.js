59bbdc9aaa412b8f0174709732c6f5b8c86231c6
SakLearn <SakLearn@1312@>
2023-09-08 17:06:15 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const show = document.querySelector(".show");
+const btn = document.querySelectorAll("button");
+const specialChars=["%","*","/","+","-","="];
+let output = ""
+
+const calculate=(btnValue) => {
+    if(btnValue === "=" && output !==""){
+        output = eval(output.replace("%","/100"));
+    }else if(btnValue === "AC"){
+        output = "";
+    }else if(btnValue === "DEL"){
+        output = output.toString().slice(0,-1);
+    }else{
+        if(output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
+    }
+    show.value = output;
+};
+
+btn.forEach((button)=>{
+    button.addEventListener("click",(e) => calculate(e.target.dataset.value));
+});
